7c822dc586d111d5766188ef963dedba6981a3ed
NABUCODONOS0R <105180633+NABUCODONOS0R@users.noreply.github.com>
2022-06-14 15:13:01 -0300

    Add files via upload
    olá mundo windows forms

---
+++ olá mundo windows forms/olá mundo windows forms/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace olá_mundo_windows_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCliqueAqui = new System.Windows.Forms.Button();
+            this.LblTextoBoaVinda = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnCliqueAqui
+            // 
+            this.BtnCliqueAqui.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BtnCliqueAqui.Location = new System.Drawing.Point(288, 197);
+            this.BtnCliqueAqui.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BtnCliqueAqui.Name = "BtnCliqueAqui";
+            this.BtnCliqueAqui.Size = new System.Drawing.Size(204, 92);
+            this.BtnCliqueAqui.TabIndex = 0;
+            this.BtnCliqueAqui.Text = "CLIQUE AQUI";
+            this.BtnCliqueAqui.UseVisualStyleBackColor = false;
+            this.BtnCliqueAqui.Click += new System.EventHandler(this.BtnCliqueAqui_Click);
+            this.BtnCliqueAqui.MouseEnter += new System.EventHandler(this.BtnCliqueAqui_MouseEnter);
+            // 
+            // LblTextoBoaVinda
+            // 
+            this.LblTextoBoaVinda.AutoSize = true;
+            this.LblTextoBoaVinda.Location = new System.Drawing.Point(340, 118);
+            this.LblTextoBoaVinda.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LblTextoBoaVinda.Name = "LblTextoBoaVinda";
+            this.LblTextoBoaVinda.Size = new System.Drawing.Size(88, 19);
+            this.LblTextoBoaVinda.TabIndex = 1;
+            this.LblTextoBoaVinda.Text = "AquiETexto";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 537);
+            this.Controls.Add(this.LblTextoBoaVinda);
+            this.Controls.Add(this.BtnCliqueAqui);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OlaMundo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCliqueAqui;
+        private System.Windows.Forms.Label LblTextoBoaVinda;
+    }
+}
+
---
+++ olá mundo windows forms/olá mundo windows forms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace olá_mundo_windows_forms //na guia do forms duplo clique no botton
+                                  //abre esse código.
+{
+    public partial class Form1 : Form  //form 1 está herdando de form, que é particionado porque será criado várias telas
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCliqueAqui_Click(object sender, EventArgs e) //o botton clique aqui vai disparar um evento de clique e possui uma aasinatura object sender que dispara um evento args
+        {
+            LblTextoBoaVinda.Text = "OLÁ IMUNDO"; //aparece essa mensagem quando o botão é clicado
+            this.BtnCliqueAqui.Text = "Exemplo 2"; //muda o texto do botão após ele ser clicado.
+
+        }
+
+        private void BtnCliqueAqui_MouseEnter(object sender, EventArgs e)
+        {
+            BtnCliqueAqui.Text = "exemplo";//mudar o texto dentro do botão
+            //BtnCliqueAqui.BackColor = System.Drawing.Color.Red; //muda a cor do botão
+            //BtnCliqueAqui.BackColor = Color.FromArgb(255, 255, 0); //não sei o porquê, mas deixa o botão amarelo
+        }
+    }
+}
