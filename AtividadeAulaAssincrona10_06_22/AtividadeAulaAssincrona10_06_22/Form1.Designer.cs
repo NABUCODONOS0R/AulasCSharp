ac7c8c04804a7ff7a14efe86a717b30d584dfc1f
NABUCODONOS0R <105180633+NABUCODONOS0R@users.noreply.github.com>
2022-06-18 21:28:57 -0300

    Add files via upload

---
+++ AtividadeAulaAssincrona10_06_22/AtividadeAulaAssincrona10_06_22/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿using System;
+
+namespace AtividadeAulaAssincrona10_06_22
+{
+    partial class AtvAula10_06
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LblSoma1 = new System.Windows.Forms.Label();
+            this.LblSoma2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ResSoma = new System.Windows.Forms.NumericUpDown();
+            this.ResSub = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblSub2 = new System.Windows.Forms.Label();
+            this.LblSub1 = new System.Windows.Forms.Label();
+            this.ResMult = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LblMult2 = new System.Windows.Forms.Label();
+            this.LblMult1 = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ResSoma)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResSub)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResMult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resolva as oprações abaixo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // LblSoma1
+            // 
+            this.LblSoma1.AutoSize = true;
+            this.LblSoma1.BackColor = System.Drawing.SystemColors.Control;
+            this.LblSoma1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSoma1.Location = new System.Drawing.Point(42, 97);
+            this.LblSoma1.Name = "LblSoma1";
+            this.LblSoma1.Size = new System.Drawing.Size(74, 29);
+            this.LblSoma1.TabIndex = 1;
+            this.LblSoma1.Text = "Num1";
+            this.LblSoma1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LblSoma1.UseMnemonic = false;
+            // 
+            // LblSoma2
+            // 
+            this.LblSoma2.AutoSize = true;
+            this.LblSoma2.BackColor = System.Drawing.SystemColors.Control;
+            this.LblSoma2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSoma2.Location = new System.Drawing.Point(264, 97);
+            this.LblSoma2.Name = "LblSoma2";
+            this.LblSoma2.Size = new System.Drawing.Size(74, 29);
+            this.LblSoma2.TabIndex = 2;
+            this.LblSoma2.Text = "Num2";
+            this.LblSoma2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LblSoma2.UseMnemonic = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(186, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "+";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.UseMnemonic = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(408, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 29);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "=";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label4.UseMnemonic = false;
+            // 
+            // ResSoma
+            // 
+            this.ResSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResSoma.Location = new System.Drawing.Point(548, 99);
+            this.ResSoma.Name = "ResSoma";
+            this.ResSoma.Size = new System.Drawing.Size(120, 29);
+            this.ResSoma.TabIndex = 5;
+            // 
+            // ResSub
+            // 
+            this.ResSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResSub.Location = new System.Drawing.Point(548, 171);
+            this.ResSub.Name = "ResSub";
+            this.ResSub.Size = new System.Drawing.Size(120, 29);
+            this.ResSub.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(408, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "=";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.UseMnemonic = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(186, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "-";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label5.UseMnemonic = false;
+            // 
+            // LblSub2
+            // 
+            this.LblSub2.AutoSize = true;
+            this.LblSub2.BackColor = System.Drawing.SystemColors.Control;
+            this.LblSub2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSub2.Location = new System.Drawing.Point(264, 169);
+            this.LblSub2.Name = "LblSub2";
+            this.LblSub2.Size = new System.Drawing.Size(74, 29);
+            this.LblSub2.TabIndex = 7;
+            this.LblSub2.Text = "Num2";
+            this.LblSub2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LblSub2.UseMnemonic = false;
+            // 
+            // LblSub1
+            // 
+            this.LblSub1.AutoSize = true;
+            this.LblSub1.BackColor = System.Drawing.SystemColors.Control;
+            this.LblSub1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSub1.Location = new System.Drawing.Point(42, 169);
+            this.LblSub1.Name = "LblSub1";
+            this.LblSub1.Size = new System.Drawing.Size(74, 29);
+            this.LblSub1.TabIndex = 6;
+            this.LblSub1.Text = "Num1";
+            this.LblSub1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LblSub1.UseMnemonic = false;
+            // 
+            // ResMult
+            // 
+            this.ResMult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResMult.Location = new System.Drawing.Point(548, 236);
+            this.ResMult.Name = "ResMult";
+            this.ResMult.Size = new System.Drawing.Size(120, 29);
+            this.ResMult.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(408, 234);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 29);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "=";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label8.UseMnemonic = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.Control;
+            this.label9.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(186, 234);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 29);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "*";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label9.UseMnemonic = false;
+            // 
+            // LblMult2
+            // 
+            this.LblMult2.AutoSize = true;
+            this.LblMult2.BackColor = System.Drawing.SystemColors.Control;
+            this.LblMult2.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMult2.Location = new System.Drawing.Point(264, 234);
+            this.LblMult2.Name = "LblMult2";
+            this.LblMult2.Size = new System.Drawing.Size(74, 29);
+            this.LblMult2.TabIndex = 12;
+            this.LblMult2.Text = "Num2";
+            this.LblMult2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LblMult2.UseMnemonic = false;
+            // 
+            // LblMult1
+            // 
+            this.LblMult1.AutoSize = true;
+            this.LblMult1.BackColor = System.Drawing.SystemColors.Control;
+            this.LblMult1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMult1.Location = new System.Drawing.Point(42, 234);
+            this.LblMult1.Name = "LblMult1";
+            this.LblMult1.Size = new System.Drawing.Size(74, 29);
+            this.LblMult1.TabIndex = 11;
+            this.LblMult1.Text = "Num1";
+            this.LblMult1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.LblMult1.UseMnemonic = false;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.Location = new System.Drawing.Point(258, 326);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(175, 36);
+            this.btnConfirmar.TabIndex = 16;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // AtvAula10_06
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.ResMult);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LblMult2);
+            this.Controls.Add(this.LblMult1);
+            this.Controls.Add(this.ResSub);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LblSub2);
+            this.Controls.Add(this.LblSub1);
+            this.Controls.Add(this.ResSoma);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LblSoma2);
+            this.Controls.Add(this.LblSoma1);
+            this.Controls.Add(this.label1);
+            this.Name = "AtvAula10_06";
+            this.Text = "Atividade Aula Assíncrona 10-06-2022";
+            ((System.ComponentModel.ISupportInitialize)(this.ResSoma)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResSub)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ResMult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LblSoma1;
+        private System.Windows.Forms.Label LblSoma2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown ResSoma;
+        private System.Windows.Forms.NumericUpDown ResSub;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LblSub2;
+        private System.Windows.Forms.Label LblSub1;
+        private System.Windows.Forms.NumericUpDown ResMult;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label LblMult2;
+        private System.Windows.Forms.Label LblMult1;
+        private System.Windows.Forms.Button btnConfirmar;
+
+        Random random = new Random();
+
+        int somar1, somar2, sub1, sub2, mult1, mult2;
+
+        public void StartQ()
+        {
+            somar1 = random.Next(1, 30);
+            somar2 = random.Next(1, 30);
+            LblSoma1.Text = somar1.ToString();
+            LblSoma2.Text = somar2.ToString();
+            ResSoma.Value = 0;
+
+
+            sub1 = random.Next(1, 30);
+            sub2 = random.Next(1, 30);
+            LblSub1.Text = sub1.ToString();
+            LblSub2.Text = sub2.ToString();
+            ResSub.Value = 0;
+
+
+            mult1 = random.Next(1, 6);
+            mult2 = random.Next(1, 6);
+            LblMult1.Text = mult1.ToString();
+            LblMult2.Text = mult2.ToString();
+            ResMult.Value = 0;
+
+            
+
+        }
+
+        private bool ChecarRespostas()
+        {
+            if ((somar1 + somar2 == ResSoma.Value) && (sub1 - sub2 == ResSub.Value) && (mult1 * mult2 == ResMult.Value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+
+        }
+
+       
+    }
+}
+
---
+++ AtividadeAulaAssincrona10_06_22/AtividadeAulaAssincrona10_06_22/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtividadeAulaAssincrona10_06_22
+{
+    public partial class AtvAula10_06 : Form
+    {
+        public AtvAula10_06()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            StartQ();
+            btnConfirmar.Enabled = false;
+
+            if (ChecarRespostas() == true)
+            {
+                MessageBox.Show("Parabéns! Você conseguiu");
+                btnConfirmar.Enabled = true;
+                
+            }
+            else
+            {
+                MessageBox.Show("Suas respostas estão incorretas. ");
+                btnConfirmar.Enabled = true;
+            }
+            
+        }
+    }
+}
